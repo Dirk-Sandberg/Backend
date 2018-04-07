2141e5486b32c7fa35136e1d6a081c88fbe8af3d
FiveToFight <37519914+FiveToFight@users.noreply.github.com>
2018-04-07 08:00:59 -0700

    Add files via upload
    need to choose earliest match played by the two players for AoE

--- VooblyMatchHistory.py
+++ VooblyMatchHistory.py
@@ -11,7 +11,7 @@ apiURL = "http://www.voobly.com/api/validate?key="+apiKey
 import requests
 import voobly
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class VooblyScraper():
@@ -35,61 +35,87 @@ class VooblyScraper():
         postRequest = self.s.post(self.authUrl, data=self.loginInfo)
         
     def checkHistory(self,player,opponent,earliestPossibleDate,LatestPossibleDate):
-        print("Match  dates don't seem to match up with what's on Voobly -- which one is correct?!")
-        print("Figured it out -- scraping data gives UTC time, while web page shows local time.")
+        print("scraping data gives UTC time, while web page shows local time.")
         print("Need to account for that when checking if date is within timeframe")
-
+        vooblyResponse = self.voobly.find_user(player)
+        playerIdNumberFromVoobly = vooblyResponse.split("\n")[1].split(",")[0]
+        notPassedDate = True # Changes to false if match history is so far back in time it's past the start of the tournament
+        pageCounter = 0 # increments based on the match history page number
+        pagePart1 = "https://www.voobly.com/profile/view/"+ playerIdNumberFromVoobly + "/Matches/games/matches/user/"+playerIdNumberFromVoobly+"/0/"
+        pagePart2 = str(pageCounter) + "#pagebrowser1"
+        matchUrl = pagePart1 + pagePart2
         playersWhoBeatThisGuy = []
         losers = []
         matchDates = []
-        vooblyResponse = self.voobly.find_user(player)
-        playerIdNumberFromVoobly = vooblyResponse.split("\n")[1].split(",")[0]
-        getRequest = self.s.get("https://www.voobly.com/profile/view/"+ playerIdNumberFromVoobly + "/Matches", params=params)
-        self.test = getRequest.content
-        soup = BeautifulSoup(getRequest.content, "html.parser") # parsed html
-        print("Will need to search through all match history in the future, rather than just the first page.")
+        datesAgainstCorrectOpponent = [] # If these guys played more than one match, find the EARLIEST match that is within the specified time period
 
-        main thing to do: scrape all match history pages until date is outside of range (+1)
-        allTDs = soup.findAll("td")
-        for i, td in enumerate(allTDs):
-            if "has won" in td.text:
-                # Get the winner
-                for a in td.findAll("a"):
-                    if ( a.text[0] != "[" ) and ( a.text[-1] != "]"): # Don't want clan tags
-                        playersWhoBeatThisGuy.append(a.text)
-                # Get the date
-                # date is in TD before the "has won" TD
-                matchDate = allTDs[i-1].text
-                print(matchDate)
-                matchDate = self.convertDate(matchDate) # Correct format
-                print(matchDate)
-                matchDates.append(matchDate)
-                # Get the other player (loser)
-                loser = allTDs[i+1].text
-                losers.append(loser)
-                # -- If "OPPONENT" not in loser or winners: they didn't play!
-                    
-                    
-            lastTd = td
+        while notPassedDate:
+            pageHasMatchHistory = False
+            getRequest = self.s.get(matchUrl)
+            soup = BeautifulSoup(getRequest.content, "html.parser") # parsed html
+            print("SOUPING " + matchUrl)
+            #main thing to do: scrape all match history pages until date is outside of range (+1)
+            allTDs = soup.findAll("td")
+            for i, td in enumerate(allTDs):
+                if "has won" in td.text:
+                    pageHasMatchHistory = True
+                    # Get the winner
+                    for a in td.findAll("a"):
+                        if ( a.text[0] != "[" ) and ( a.text[-1] != "]"): # Don't want clan tags
+                            # Get the other player (loser)
+                            loser = allTDs[i+1].text
+                            
+                            # Get the date
+                            # date is in TD before the "has won" TD
+                            self.test1 = soup
+                            self.test2 = matchUrl
+                            matchDate = allTDs[i-1].text                            
+                            matchDate = self.convertDate(matchDate) # Correct format
+                            dateIsInsideMargin = self.checkIfDateIsInsideMargin(matchDate,earliestPossibleDate,LatestPossibleDate)
+                            if ( dateIsInsideMargin == -1 ):
+                                print(matchDate)
+                                print("DATE IS TOO EARLY \n\n")
+                                notPassedDate = False
+                                break # Stop searching match history by breaking while loop. This date is too far back in time
+                            if ( dateIsInsideMargin == 0): # 0 means it IS inside the margin
+                                playersWhoBeatThisGuy.append(a.text)
+                                matchDates.append(matchDate)
+                                losers.append(loser)
+                            # -- If "OPPONENT" not in loser or winners: they didn't play!                                                                
+                lastTd = td
+            if ( not pageHasMatchHistory):
+                # Stop searching match history by breaking while loop. The player hasn't played any matches earlier than what's displayed on this/the previous page
+                break 
+            pageCounter += 1 # Move to next page (i.e. 0/pageCounter#browser1 )
+            pagePart2 = str(pageCounter+1) + pagePart2[pagePart2.find("#"):] # Replace first character
+            matchUrl = pagePart1 + pagePart2
+        self.test1 = losers
         self.test2 = playersWhoBeatThisGuy
         self.test3 = matchDates
         print("Need to check to make sure this was the only match they played.")
         print("Otherwise, need to check all matches and get the first one played  in the timeframe allotted for the match!")
 
+        # rightIndexes = np.argsort(matchDates)
+        # rightindexedOpponents = opponents[rightindexes]
+        # rightindexedwinners = wiiners[rightindexes]
+        
         if (opponent not in playersWhoBeatThisGuy) and (opponent not in losers):
             return "N/A"
         for i, winner in enumerate(playersWhoBeatThisGuy):
             if winner in opponent: # If the winner of this match was the selected opponent for this bracket
                 dateIsInsideMargin = self.checkIfDateIsInsideMargin(matchDates[i],earliestPossibleDate,LatestPossibleDate)
-                if dateIsInsideMargin:
-                    return opponent # opponent was a winner
+                if ( dateIsInsideMargin == 0 ):
+                    print("Doesn't check for earliest game played in the interval yet")
+                    return opponent
+                    #datesAgainstCorrectOpponent.append(datetime.strptime(matchDates[i],"%Y-%m-%d"))#return opponent # opponent was a winner
+        
         return player # If no opponent was returned, the player won
     
     def close(self):
         self.s.close()
         
 
-    def checkIfDateIsINsideMargin(date,left,right):
+    def checkIfDateIsInsideMargin(self,date,left,right):
         # all dates have form yyyy-mm-dd
         left = datetime.strptime(left,  "%Y-%m-%d")
         date = datetime.strptime(date,  "%Y-%m-%d")
@@ -106,15 +132,22 @@ class VooblyScraper():
         '''
         Converts date format from Voobly (6 March 2018) to standard (yyyy-mm-dd)
         '''
-        splitDate = date.split(" ")
-        months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-        day = splitDate[0]
-        month = splitDate[1]
-        year = splitDate[2]
-        month = months.index(month) + 1
-        month = str(month)
-        if len(month) != 2:
-            month = "0" + month
-        if len(day) != 2:
-            day = "0" + day
-        return "-".join([year,month,day])
+        self.test = date
+        if "Today" in date:
+            return datetime.today().strftime("%Y-%m-%d")
+        if "Yesterday" in date:
+            yesterday = datetime.today() - timedelta(1)
+            return yesterday.strftime("%Y-%m-%d")        
+        else:
+            splitDate = date.split(" ")
+            months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+            day = splitDate[0]
+            month = splitDate[1]
+            year = splitDate[2]
+            month = months.index(month) + 1
+            month = str(month)
+            if len(month) != 2:
+                month = "0" + month
+                if len(day) != 2:
+                    day = "0" + day
+            return "-".join([year,month,day])
