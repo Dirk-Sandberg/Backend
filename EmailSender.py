ec3f01f00d527e9e9ff75b8fc596dac978a33979
FiveToFight <37519914+FiveToFight@users.noreply.github.com>
2018-04-08 12:24:34 -0700

    Add files via upload
    Gmail API -- scripts to send emails from fivetofight account

---
+++ EmailSender.py
@@ -0,0 +1,131 @@
+'''
+send emails using python and Gmail API
+'''
+from __future__ import print_function
+from email.mime.text import MIMEText
+from email.mime.multipart import  MIMEMultipart
+import base64
+
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/gmail-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    msg = create_message_without_attachment("esandberg@fivetofight.com","eriksandbergum@gmail.com","Hi",r'Hi<br/>Html <b>hello HTML</b>', "Hello plain!!")
+    send_message(service,"me",msg)
+
+
+def create_message(sender, to, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+def create_message_without_attachment (sender, to, subject, message_text_html, message_text_plain):
+    #Create message container
+    message = MIMEMultipart('alternative') # needed for both plain & HTML (the MIME type is multipart/alternative)
+    message['Subject'] = subject
+    message['From'] = sender
+    message['To'] = to
+
+    #Create the body of the message (a plain-text and an HTML version)
+    message.attach(MIMEText(message_text_plain, 'plain'))
+    message.attach(MIMEText(message_text_html, 'html'))
+
+    raw_message_no_attachment = base64.urlsafe_b64encode(message.as_bytes())
+    raw_message_no_attachment = raw_message_no_attachment.decode()
+    body  = {'raw': raw_message_no_attachment}
+    return body
+
+def send_message(service, user_id, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print('Message Id: %s' % message['id'])
+  except Exception as error:
+    print('An error occurred: %s' % error)
+
+
+'''
+End of function definitions!
+'''
+
+main()
