c020719e7e0da77378d6fdbf4268fcc6522bafec
LitterBeach <35900848+LitterBeach@users.noreply.github.com>
2018-05-19 16:35:19 -0500

    first attempt on refactoring

--- Customer.py
+++ Customer.py
@@ -5,15 +5,13 @@ Created on Wed May 16 20:44:41 2018
 @author: Jiaming Fu
 """
 
-class Customer():
-    def __init__(self, email, firstName, lastName, billingAddress, phone, paypal, username):
+
+class Customer:
+    def __init__(self, email, first_name, last_name, billing_address, phone, paypal, username):
         self.email = email
-        self.firstName = firstName
-        self.lastName = lastName
-        self.billingAddress = billingAddress
+        self.first_name = first_name
+        self.last_name = last_name
+        self.billing_address = billing_address
         self.phone = phone
         self.paypal = paypal
-        self.username =username
-        
-        
-    
+        self.username = username
--- Order.py
+++ Order.py
@@ -5,16 +5,15 @@ Created on Wed May 16 21:05:18 2018
 @author: Jiaming Fu
 """
 
-class Order(): 
-    def __init__(self, customerEmail, billingAddress, formSubmission, ID, orderNumber, createdOn, subtotal, lineItems ):
-        self.customerEmail = customerEmail
-        self.billingAddress = billingAddress
-        self.formSubmission = formSubmission
-        self.ID = ID
-        self.orderNumber = orderNumber
-        self.createdOn = createdOn
+
+class Order:
+    def __init__(self, customer_email, billing_address, form_submission, order_id, order_number, created_on, subtotal,
+                 line_items):
+        self.customer_email = customer_email
+        self.billing_address = billing_address
+        self.form_submission = form_submission
+        self.order_id = order_id
+        self.order_number = order_number
+        self.created_on = created_on
         self.subtotal = subtotal
-        self.lineItems = lineItems
-        
-    
-    
+        self.line_items = line_items
